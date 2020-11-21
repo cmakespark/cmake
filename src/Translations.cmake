280128225f02bc87dcf269097d0562ba5e8cc797
frederik vannoote <frederik.vannoote@barco.com>
2020-11-21 10:28:50 +0100

    Rework how ts files are produced.

--- src/Translations.cmake
+++ src/Translations.cmake
@@ -15,9 +15,20 @@ if(Qt5Core_FOUND AND Qt5LinguistTools_FOUND)
         list(APPEND TRANSLATION_OUTPUT_FILES "${TRANSLATIONS_OUTPUT_DIR}/${LOCALE}.ts")
     endforeach()
 
+    set_source_files_properties(${TRANSLATION_OUTPUT_FILES} PROPERTIES OUTPUT_LOCATION ${TRANSLATIONS_OUTPUT_DIR})
+
     # Generate TS and QM files
-    qt5_create_translation(QM_FILES ${CMAKE_SOURCE_DIR} ${TRANSLATION_OUTPUT_FILES})
+    # message(STATUS "TRANSLATION_OUTPUT_FILES: ${TRANSLATION_OUTPUT_FILES}")
+    qt5_create_translation(QM_FILES ${CMAKE_SOURCE_DIR} ${TRANSLATION_OUTPUT_FILES} OPTIONS -no-obsolete)
+    message(STATUS "QM_FILES: ${QM_FILES}")
 
     # Create build target
-    add_custom_target(translate DEPENDS ${QM_FILES})
+    add_custom_target(translate
+#        COMMAND ${Qt5_LUPDATE_EXECUTABLE} . -ts ${TRANSLATION_OUTPUT_FILES} -no-obsolete
+        DEPENDS ${QM_FILES}
+        WORKING_DIRECTORY ${CMAKE_SOURCE_DIR})
+
+    add_custom_target(translate-clean
+        COMMAND ${CMAKE_COMMAND} -E remove ${QM_FILES})
+
 endif(Qt5Core_FOUND AND Qt5LinguistTools_FOUND)
